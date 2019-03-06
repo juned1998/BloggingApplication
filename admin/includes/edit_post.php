83c90a70d15ea4c7ab01edbff5ba43d504781fac
Juned Khan <junaidkhanhere@gmail.com>
2019-03-06 21:25:17 +0530

    Update edit_post.php

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,46 +1,26 @@
 
 <?php
    
+if(isset($_GET['p_id'])){
+$query = "SELECT * from posts";
+$the_post_id = $_GET['p_id'];
+}
+        $select_posts_by_id= mysqli_query($connection , $query);
 
-   if(isset($_POST['create_post'])) {
-   
-            $post_title        = escape($_POST['title']);
-            $post_user         = escape($_POST['post_user']);
-            $post_category_id  = escape($_POST['post_category']);
-            $post_status       = escape($_POST['post_status']);
+        while($row = mysqli_fetch_assoc($select_posts_by_id))
+        {
+        $post_id = $row['post_id'];
+         $post_author = $row['post_author'];  
+         $post_title = $row['post_title'];      
+         $post_category_id = $row['post_category_id'];  
+         $post_status = $row['post_status'];  
+         $post_image = $row['post_image'];  
+        $post_tags = $row['post_tags']; 
+        $post_comment_count = $row['post_comment_count'];
+          $post_date = $row['post_date'];
     
-            $post_image        = escape($_FILES['image']['name']);
-            $post_image_temp   = escape($_FILES['image']['tmp_name']);
-    
-    
-            $post_tags         = escape($_POST['post_tags']);
-            $post_content      = escape($_POST['post_content']);
-            $post_date         = escape(date('d-m-y'));
-
-       
-        move_uploaded_file($post_image_temp, "../images/$post_image" );
-       
-       
-      $query = "INSERT INTO posts(post_category_id, post_title, post_user, post_date,post_image,post_content,post_tags,post_status) ";
-             
-      $query .= "VALUES({$post_category_id},'{$post_title}','{$post_user}',now(),'{$post_image}','{$post_content}','{$post_tags}', '{$post_status}') "; 
-             
-      $create_post_query = mysqli_query($connection, $query);  
-          
-      confirmQuery($create_post_query);
-
-      $the_post_id = mysqli_insert_id($connection);
-
-
-      echo "<p class='bg-success'>Post Created. <a href='../post.php?p_id={$the_post_id}'>View Post </a> or <a href='posts.php'>Edit More Posts</a></p>";
-       
-
-
-   }
-    
-
-    
-    
+        }
+}
 ?>
 
     <form action="" method="post" enctype="multipart/form-data">    
@@ -53,80 +33,29 @@
 
          <div class="form-group">
        <label for="category">Category</label>
-       <select name="post_category" id="">
-           
-<?php
-
-        $query = "SELECT * FROM categories";
-        $select_categories = mysqli_query($connection,$query);
-        
-        confirmQuery($select_categories);
-
-
-        while($row = mysqli_fetch_assoc($select_categories )) {
-        $cat_id = $row['cat_id'];
-        $cat_title = $row['cat_title'];
-            
-            
-            echo "<option value='$cat_id'>{$cat_title}</option>";
-         
-            
-        }
-
-?>
-           
-        
-       </select>
-      
+    <input type="text" class="form-control" name="post_category">
       </div>
+
+
 
 
        <div class="form-group">
-       <label for="users">Users</label>
-       <select name="post_user" id="">
-           
-<?php
-
-        $users_query = "SELECT * FROM users";
-        $select_users = mysqli_query($connection,$users_query);
-        
-        confirmQuery($select_users);
-
-
-        while($row = mysqli_fetch_assoc($select_users)) {
-        $user_id = $row['user_id'];
-        $username = $row['username'];
-            
-            
-            echo "<option value='{$username}'>{$username}</option>";
-         
-            
-        }
-
-?>
-           
-        
-       </select>
-      
-      </div>
-
-
-
-
-
-      <!-- <div class="form-group">
          <label for="title">Post Author</label>
           <input type="text" class="form-control" name="author">
-      </div> -->
+      </div> 
       
       
 
        <div class="form-group">
+<!--
          <select name="post_status" id="">
              <option value="draft">Post Status</option>
              <option value="published">Published</option>
              <option value="draft">Draft</option>
          </select>
+-->
+           <label for="title">Post Status</label>
+          <input type="text" class="form-control" name="author">
       </div>
       
       
