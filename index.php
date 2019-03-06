05338d935f1c8e538e7af4295c76854d1f4c9cc8
Juned Khan <junaidkhanhere@gmail.com>
2019-03-06 23:47:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,10 +11,14 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
               <?php
+                
+               
+                
                     $query = "SELECT * from posts";
 $select_all_posts_query = mysqli_query($connection , $query);
             while($row = mysqli_fetch_assoc($select_all_posts_query))
             {
+                $post_id = $row['post_id'];
                 $post_title = $row['post_title'];
                  $post_author = $row['post_author'];
                  $post_date = $row['post_date'];
@@ -29,7 +33,7 @@ $select_all_posts_query = mysqli_query($connection , $query);
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#"><?php echo $post_title; ?></a>
+                    <a href="post.php?p_id=<?php echo $post_id ;?>"><?php echo $post_title; ?></a>
                 </h2>
                 <p class="lead">
                     by <a href="index.php"><?php echo $post_author; ?></a>
