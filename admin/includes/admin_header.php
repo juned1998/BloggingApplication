302375452e51b6b974f56f82e982456970465f97
Juned Khan <junaidkhanhere@gmail.com>
2019-03-08 23:16:34 +0530

    Update admin_header.php

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,5 +1,19 @@
-<?php ob_start(); ?>
 <?php include_once"../includes/db.php";?>
+<?php ob_start(); ?>
+<?php session_start();?>
+<?php
+    if(isset($_SESSION['user_role'])){
+        $user_role = $_SESSION['user_role'];
+        if($user_role!=='admin'){
+
+            header("Location:../index.php");
+        }
+        
+    }
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
