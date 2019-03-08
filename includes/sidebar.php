237f959476709d93ae5936fa5dd3f54eb9e5704c
Juned Khan <junaidkhanhere@gmail.com>
2019-03-08 21:55:22 +0530

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -16,6 +16,32 @@
                     </form><!--form end-->    
                     <!-- /.input-group -->
                 </div>
+     
+     
+     
+<!-- Login form -->
+    <div class="well">
+        <h4>Login</h4>
+        <form action="includes/login.php" method="post">
+        <div class="form-group">
+            <input placeholder="Enter Username" name="username" type="text" class="form-control">   
+        </div>
+            
+        <div class="input-group">
+            <input placeholder="Enter Password" name="password" type="password" class="form-control">
+            <span class="input-group-btn">
+                <button class="btn btn-primary" name="login" type="submit">
+                    Submit
+                </button>
+            </span>
+            
+        </div>    
+        </form><!--form end-->    
+        <!-- /.input-group -->
+    </div>
+
+     
+     
 
                 <!-- Blog Categories Well -->   
                 <div class="well">
