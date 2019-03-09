be632d1f76120cb64438b100ce2b9509c328c22c
Juned Khan <junaidkhanhere@gmail.com>
2019-03-09 23:37:08 +0530

    Update admin_footer.php

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -6,6 +6,18 @@
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
+    
+<!-- Include the Quill library -->
+<!--<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>-->
+
+<script src="../js/quill-1.3.6/quill.js"> </script>
+
+<!-- Initialize Quill editor -->
+<script>
+  var quill = new Quill('#editor', {
+    theme: 'snow'
+  });
+</script>
 
 </body>
 
