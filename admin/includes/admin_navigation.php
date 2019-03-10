5fd036afa9408f93fa042246260e9af2a60e179d
Juned Khan <junaidkhanhere@gmail.com>
2019-03-10 22:03:23 +0530

    Update admin_navigation.php

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -1,4 +1,4 @@
-       <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
@@ -11,10 +11,22 @@
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
+<!--              <li><a href="">Users online:<?php //echo users_online(); ?></a></li>-->          <li><a href="">Users online:<span class="usersonline"></span></a></li>
                 <li><a href="../index.php">Home Website</a></li>
+                
        
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>
+                        <?php
+                        
+                        if(isset($_SESSION['username'])){
+                         echo $_SESSION['username'];}
+                        
+                        ?>
+         
+       
+                        
+                        <b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li>
                             <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
