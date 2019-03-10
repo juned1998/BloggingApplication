420906c6571baa086bd13088e807d9d925f849f7
Juned Khan <junaidkhanhere@gmail.com>
2019-03-10 22:03:19 +0530

    Update view_all_posts.php

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -12,6 +12,23 @@ if(isset($_POST['checkBoxArray'])) {
   $bulk_options = $_POST['bulk_options'];
         
         switch($bulk_options) {
+                
+                
+          case 'reset_views':
+        
+        $query = "UPDATE posts SET post_views_count = 0 WHERE post_id =".mysqli_real_escape_string($connection,$postValueId)." ";
+        
+        $reset_query = mysqli_query($connection , $query);       
+                confirmQuery($reset_query);
+
+
+            
+         break;        
+                
+        
+                
+                
+                
         case 'published':
         
 $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$postValueId}  ";
@@ -118,6 +135,7 @@ confirmQuery($update_to_delete_status);
         <option value="draft">Draft</option>
         <option value="delete">Delete</option>
          <option value="clone">Clone</option>
+        <option value="reset_views">Reset Views count</option>            
         </select>
 
         </div> 
@@ -135,16 +153,19 @@ confirmQuery($update_to_delete_status);
                 <thead>
                     <tr>
                 <th><input id="selectAllBoxes" type="checkbox"></th>
+                          <th>Post ID</th>
                           <th>Author</th>
                           <th>Title</th>
                           <th>Category</th>
                           <th>Status</th>
                           <th>Image</th>
                           <th>Tags</th>
-                        <th>Comments</th>
+                        <th>Comments count</th>
                         <th>Date</th>
+                        <th>View post</th>
                         <th>Edit</th>
                         <th>Delete</th>
+                        <th>Post views</th>
                         
                     </tr>
                     </thead>
@@ -165,6 +186,7 @@ confirmQuery($update_to_delete_status);
         $post_tags = $row['post_tags']; 
         $post_comment_count = $row['post_comment_count'];
           $post_date = $row['post_date'];
+          $post_views_count = $row['post_views_count'];
             
         echo "<tr>";?>
  <td><input class='checkBoxes' type='checkbox' name='checkBoxArray[]' value='<?php echo $post_id; ?>'></td>                        
@@ -192,8 +214,10 @@ confirmQuery($update_to_delete_status);
             echo "<td>{$post_tags}</td>";
             echo "<td>{$post_comment_count}</td>";
             echo "<td>{$post_date}</td>";
+    echo "<td><a href='../post.php?&p_id={$post_id}'>View Post</a></td>";        
     echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
-    echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
+    echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete');\" href='posts.php?delete={$post_id}'>Delete</a></td>";
+echo "<td><a href='posts.php?reset={$post_id}'>$post_views_count</a></td>";
         echo "</tr>";    
         
         }
@@ -216,6 +240,13 @@ confirmQuery($update_to_delete_status);
         $the_post_id = $_GET['delete'];
         $query = "DELETE FROM posts WHERE post_id = {$the_post_id}";
         $delete_query = mysqli_query($connection , $query);
+        header("Location:posts.php");
+    }
+    
+  if(isset($_GET['reset'])){
+        $the_post_id = $_GET['reset'];
+        $query = "UPDATE posts SET post_views_count = 0 WHERE post_id =".mysqli_real_escape_string($connection,$the_post_id)." ";
+        $reset_query = mysqli_query($connection , $query);
         header("Location:posts.php");
     }
 
