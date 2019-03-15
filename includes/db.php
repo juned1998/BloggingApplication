8407fcf948c6202c35917b7431a45ec02d0c97bc
Juned Khan <junaidkhanhere@gmail.com>
2019-03-15 22:52:08 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
 define("DB_HOST",'localhost');
 define("DB_USER",'root');
-define("DB_PASS",'root');
-define("DB_NAME",'cmsNew');
+define("DB_PASS",'');
+define("DB_NAME",'cms');
 
 //Connecting to database
 $connection = mysqli_connect(DB_HOST , DB_USER, DB_PASS,DB_NAME);
