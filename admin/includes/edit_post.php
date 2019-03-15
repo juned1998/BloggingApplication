7d71d759ea181d503d655489351675cc31caf45c
Juned Khan <junaidkhanhere@gmail.com>
2019-03-15 22:52:03 +0530

    Update edit_post.php

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -160,7 +160,7 @@ $the_post_id = $_GET['p_id'];
       
       <div class="form-group">
          <label for="post_content">Post Content</label>
-         <textarea  class="form-control "name="post_content" id="editor" cols="30" rows="10"><?php echo $post_content;?></textarea>
+         <textarea  class="form-control "name="post_content" id="editor" cols="30" rows="10"><?php echo /*str_replace('\r\n', '</br>', */$post_content/*)*/ ;?></textarea>
       </div>
       
       
