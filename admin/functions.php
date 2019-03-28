f9637a5920eee154379ffda7d53d44e8713db1d4
Juned Khan <junaidkhanhere@gmail.com>
2019-03-29 01:38:47 +0530

    Update functions.php

--- admin/functions.php
+++ admin/functions.php
@@ -1,4 +1,121 @@
 <?php
+
+//GENERAL HELPERS START
+
+function get_user_name(){
+    return isset($_SESSION['username']) ? $_SESSION['username'] : null ;
+}
+//GENERAL HELPERS ENDS
+
+
+
+//DATABASE HELPER FUNCTION START
+function fetchRecords($result){
+    return mysqli_fetch_array($result);
+}
+
+function query($query){
+    global $connection;
+    $result = mysqli_query($connection, $query);
+    confirmQuery($result);
+    return $result;
+}
+
+
+//DATABASE HELPER FUNCTION ENDS
+
+
+//AUTHENTICATION HELPER FUNCTION START
+
+function is_admin(){
+    global $connection;
+
+    if(isLoggedIn()){
+        $query = "SELECT user_role FROM users WHERE user_id =".$_SESSION['user_id']."";
+        $result = mysqli_query($connection , $query);
+        confirmQuery($result);
+
+        $row = fetchRecords($result);
+
+        if($row['user_role']=='admin'){
+            
+            return true;
+
+        } else{
+
+            return false;
+            
+        }
+    }
+    return false;
+}
+
+function isLoggedIn(){
+    if(isset($_SESSION['user_role'])){
+        return true;
+    }
+   return false;
+}
+
+function loggedInUserId(){
+    if(isLoggedIn()){
+        $result = query("SELECT * FROM users WHERE username='" . $_SESSION['username'] ."'");
+        confirmQuery($result);
+        $user = mysqli_fetch_array($result);
+        return mysqli_num_rows($result) >= 1 ? $user['user_id'] : false;
+    }
+    return false;
+
+}
+
+
+//AUTHENTICATION HELPER FUNCTION ENDS
+
+
+//USER SPECIFIC STARTS
+
+function get_all_user_posts(){
+    return query("SELECT * FROM posts WHERE user_id=".loggedInUserId()."");
+}
+
+function get_all_posts_user_comments(){
+    return query("SELECT * FROM posts
+    INNER JOIN comments ON posts.post_id = comments.comment_post_id
+    WHERE user_id=".loggedInUserId()."");
+
+}
+
+function get_all_user_categories(){
+    return query("SELECT * FROM categories WHERE user_id=".loggedInUserId()."");
+}
+
+function get_all_user_published_posts(){
+    return query("SELECT * FROM posts WHERE user_id=".loggedInUserId()." AND post_status='published'");
+}
+
+function get_all_user_draft_posts(){
+    return query("SELECT * FROM posts WHERE user_id=".loggedInUserId()." AND post_status='draft'");
+}
+
+function get_all_user_approved_posts_comments(){
+    return query("SELECT * FROM posts
+    INNER JOIN comments ON posts.post_id = comments.comment_post_id
+    WHERE user_id=".loggedInUserId()." AND comment_status='approved'");
+}
+
+
+function get_all_user_unapproved_posts_comments(){
+    return query("SELECT * FROM posts
+    INNER JOIN comments ON posts.post_id = comments.comment_post_id
+    WHERE user_id=".loggedInUserId()." AND comment_status='unapproved'");
+}
+
+
+//USER SPECIFIC ENDS
+
+
+
+
 function redirect($location){
 
     return  header("Location:".$location);
@@ -56,12 +173,13 @@ function insert_categories(){
 global $connection;    
 if(isset($_POST['submit'])){
             $cat_title = mysqli_real_escape_string($connection,$_POST['cat_title']);
+            $user_id = loggedInUserId();
             if($cat_title == "" || empty($cat_title)){
                 echo "<h4 style = 'color:red;'>Title cannot be empty !<h4>";
             }
             else{
-                $query = "INSERT INTO categories(cat_title)";
-                $query.= "VALUE('{$cat_title}')";
+                $query = "INSERT INTO categories(cat_title , user_id)";
+                $query.= "VALUES('{$cat_title}' , '{$user_id}')";
                 $create_category_query = mysqli_query($connection,$query);
                 if(! $create_category_query){
                     die('Query Failed'.mysqli_error($connection));
@@ -74,6 +192,9 @@ if(isset($_POST['submit'])){
 function findAllCategories(){
     global $connection;
     $query = "SELECT * from categories";
+    if(!is_admin()){
+        $query = "SELECT * FROM categories WHERE user_id=".loggedInUserId()."";
+    }
 $select_categories= mysqli_query($connection , $query);
 
 while($row = mysqli_fetch_assoc($select_categories))
@@ -139,24 +260,7 @@ return  mysqli_num_rows($select_all_subscribers);
 
 
 
-function is_admin($username){
-    global $connection;
-    $query = "SELECT user_role FROM users WHERE USERNAME = '$username' ";
-    $result = mysqli_query($connection , $query);
-    confirmQuery($result);
 
-    $row = mysqli_fetch_array($result);
-
-    if($row['user_role']=='admin'){
-        
-        return true;
-
-    } else{
-
-        return false;
-        
-    }
-}
 
 
 function username_exists($username){
@@ -247,7 +351,7 @@ function login_user($username , $password){
 
     
     if(password_verify($password , $db_user_password)){
-        
+        $_SESSION['user_id'] = $db_user_id;
           $_SESSION['username'] = $db_username;
         $_SESSION['firstname'] = $db_user_firstname;
         $_SESSION['lastname'] = $db_user_lastname;
@@ -255,13 +359,15 @@ function login_user($username , $password){
         
         redirect("/cms/admin/");
         
-       
+        
         
     } else{
-        
-        redirect("/cms/index.php");
-        
+       redirect("/cms/index.php");
     }
+}
+
+function count_records($result){
+    return mysqli_num_rows($result);
 }
 
 
