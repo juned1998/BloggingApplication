a9f4f2ad78543fb8d96fcd9f186885b6d5078884
Juned Khan <junaidkhanhere@gmail.com>
2019-03-29 01:38:42 +0530

    Update comments.php

--- admin/comments.php
+++ admin/comments.php
@@ -24,13 +24,6 @@
         }
         
         switch($source){
-            case 'add_post':
-                include "includes/add_post.php";
-            break;
-                
-            case 'edit_post':
-                include "includes/edit_post.php";
-            break;    
             
             default :
                 include "includes/view_all_comments.php";
