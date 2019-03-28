72778abd979f73008d824e54c42afc1acb050a15
Juned Khan <junaidkhanhere@gmail.com>
2019-03-29 01:38:50 +0530

    Update admin_navigation.php

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -44,9 +44,15 @@
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
                     <li>
-                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> My Data</a>
                     </li>
-                    
+
+                    <?php if(is_admin()): ?>
+                    <li>
+                        <a href="dashboard.php"><i class="fa fa-fw fa-dashboard"></i>Dashboard</a>
+                    </li>
+                    <?php endif ?>
+
                     <li>
                     <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                     <ul id="posts_dropdown" class="collapse">
@@ -67,6 +73,8 @@
                     <li class="">
                         <a href="comments.php"><i class="fa fa-fw fa-file"></i> Comments</a>
                     </li>
+
+                    <?php if(is_admin()): ?>
                     <li>
                         <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Users<i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="demo" class="collapse">
@@ -78,6 +86,7 @@
                             </li>
                         </ul>
                     </li>
+                <?php endif ;?>
                     <li>
                         <a href="profile.php"><i class="fa fa-fw fa-dashboard"></i>Profile</a>
                     </li>
